fec805b9fa542bd90bf974981f3c4cfa01c8e66f
José Luan Lima da Costa <joseluan620@gmail.com>
2022-05-15 20:23:55 -0300

    componentes dinamicos e configuração de eventos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'netflix-angular';
+
+  menuOpened = false;
+
+  listTitles = [
+    {
+      trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: '/assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia 1',
+      relevance: 9,
+      age: 0,
+      parts: 2,
+      categories: ['Épico', 'Filme de fantasia']
+    },
+    {
+      trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: '/assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia 2',
+      relevance: 8,
+      age: 12,
+      parts: 2,
+      categories: ['Épico', 'Viagem no espaço']
+    },
+    {
+      trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: '/assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia 3',
+      relevance: 98,
+      age: 12,
+      parts: 2,
+      categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+    },
+    {
+      trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: '/assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia 4',
+      relevance: 70,
+      age: 16,
+      parts: 7,
+      categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
+    },
+    {
+      trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
+      cover: '/assets/images/guardios-da-galaxia.jpg',
+      name: 'Guardiões da Galaxia 5',
+      relevance: 7,
+      age: 14,
+      parts: 9,
+      categories: ['Filme de fantasia', 'Viagem no espaço']
+    }
+  ];
+
+  setMenuState(state: boolean){
+    this.menuOpened = state;
+  }
+
+  closeMenu(){
+    this.menuOpened = false;
+  }
+
 }
---
+++ src/app/shared/title/title.component.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent implements OnInit {
+
+  @Input() title: any;
+  @ViewChild('trailer', {static: true}) trailer!: ElementRef<HTMLVideoElement>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  playVideo(){
+    this.trailer.nativeElement.play();
+  }
+
+  stopVideo(){
+    this.trailer.nativeElement.currentTime = 0;
+    this.trailer.nativeElement.pause();
+  }
+
+  getMuted(): boolean{
+    return this.trailer.nativeElement.muted;
+  }
+
+  toggleSound(){
+    this.trailer.nativeElement.muted = !this.trailer.nativeElement.muted;
+  }
+
+}
