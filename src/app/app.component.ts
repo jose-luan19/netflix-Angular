1b8fe5ce5777f47bc845129cd139a9294b96524c
José Luan Lima da Costa <joseluan620@gmail.com>
2022-05-18 22:15:57 -0300

    organizando estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,15 +22,6 @@ export class AppComponent {
     {
       trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
       cover: '/assets/images/guardios-da-galaxia.jpg',
-      name: 'Guardiões da Galaxia 2',
-      relevance: 8,
-      age: 12,
-      parts: 2,
-      categories: ['Épico', 'Viagem no espaço']
-    },
-    {
-      trailer: '/assets/videos/trailer_guardioes_da_galaxia_1080p.mp4',
-      cover: '/assets/images/guardios-da-galaxia.jpg',
       name: 'Guardiões da Galaxia 3',
       relevance: 98,
       age: 12,
@@ -42,7 +33,7 @@ export class AppComponent {
       cover: '/assets/images/guardios-da-galaxia.jpg',
       name: 'Guardiões da Galaxia 4',
       relevance: 70,
-      age: 16,
+      age: 12,
       parts: 7,
       categories: ['Épico', 'Filme de fantasia', 'Viagem no espaço']
     },
